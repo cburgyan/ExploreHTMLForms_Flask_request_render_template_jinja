bb8929005c060acb375f4b294bf42aee841caafa
cburgyan <cburgyan@gmail.com>
2022-07-23 12:12:13 -0500

    display username and password in login_response.html

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login_response_page():
+    password = request.form['password']
+    username = request.form['username']
+    print(type(request))
+    print(request.data)
+    print(password)
+    return render_template('login_response.html', name=username, password=password)
 
 
 if __name__ == "__main__":
